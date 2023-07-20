f4f9518c3793b6fa391e41c1d057cb21eb14e4c7
yogendra09 <yogendrayadav168@gmail.com>
2023-07-20 13:49:45 +0530

    completed

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,13 @@ const nodemailer = require("nodemailer");
 const googleApis = require("googleapis");
 
 const REDIRECT_URI = `https://developers.google.com/oauthplayground`;
-const CLIENT_ID = `get it from developer console`;
-const CLIENT_SECRET = `get it from developer console`;
-const REFRESH_TOKEN = `get it from developer console`;
+const CLIENT_ID = `348131055928-j16hmb4nq1b7roki6jes2h27ek8ieg63.apps.googleusercontent.com`;
+const CLIENT_SECRET = `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`;
+const REFRESH_TOKEN = `1//04K5oaUzfRxYuCgYIARAAGAQSNwF-L9Irky9fS12NhT0MYb8371p-XE2QKmyGvO2nPgPmreUlm2p9OqSLPPLiFbKBjczMsNe-HBo`;
 
+const authClient = new googleApis.google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, 
+  REDIRECT_URI);
+  authClient.setCredentials({refresh_token: REFRESH_TOKEN});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,17 +19,15 @@ router.get('/', function(req, res, next) {
 
 router.post('/send-mail',function(req,res,next){
 
-  const authClient = new googleApis.google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, 
-    REDIRECT_URI);
-    authClient.setCredentials({refresh_token: REFRESH_TOKEN});
+ 
     async function mailer(){
      try{
      const ACCESS_TOKEN = await authClient.getAccessToken();
-     const transport = nodemailer.createTransport({
+     const transporter = nodemailer.createTransport({
      service: "gmail",
      auth: {
      type: "OAuth2",
-     user: "admin ka email",
+     user: "hi.yogesh09@gmail.com",
      clientId: CLIENT_ID,
      clientSecret: CLIENT_SECRET,
      refreshToken: REFRESH_TOKEN,
@@ -34,13 +35,13 @@ router.post('/send-mail',function(req,res,next){
      }
      })
      const details = {
-     from: "yogendrayadav168@gmail.com",
+     from: "hi.yogesh09@gmail.com",
      to: req.body.to,
      subject: req.body.subject,
      text: req.body.message,
-     html: "<h2>lund se</h2>"
+     html: `<h2 style="color:red">lund se</h2>`
      }
-     const result = await transport.mailer(details);
+     const result = await transporter.sendMail(details);
      return result;
      }
      catch(err){
